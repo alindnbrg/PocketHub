49e345c441b10bdc3274c88de6defed9df6cd73c
alindnbrg <mail@andrelindenberg.de>
2019-03-06 17:52:32 +0100

    updated pipeline

--- ci/run_sq_scan.sh
+++ ci/run_sq_scan.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export fly_target=${fly_target:-tutorial}
-echo "Concourse API target ${fly_target}"
-echo "Tutorial $(basename $DIR)"
+# DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# export fly_target=${fly_target:-tutorial}
+# echo "Concourse API target ${fly_target}"
+# echo "Tutorial $(basename $DIR)"
 
-# ./gradlew sonarqube \
-#   -Dsonar.projectKey=sonar_proejctKey \
-#   -Dsonar.organization=sonar_orga \
-#   -Dsonar.host.url=https://sonarcloud.io \
-#   -Dsonar.login=sonar_login
+./gradlew sonarqube \
+  -Dsonar.projectKey=sonar_proejctKey \
+  -Dsonar.organization=sonar_orga \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=sonar_login
